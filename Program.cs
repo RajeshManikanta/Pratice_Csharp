0de3d095c2c400cee433ace6780f1bf96f38b2c5
RajeshManikanta <38816842+RajeshManikanta@users.noreply.github.com>
2018-04-28 13:55:46 -0400

    Console Project
    Creation of New Console Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pratice_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
